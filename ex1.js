e1c222b508b06e794cde341b9045b79bb2ffa21d
SHIVA . <vishnushiva665@gmail.com>
2025-07-23 11:54:06 +0530

    Adding new file

---
+++ ex1.js
@@ -0,0 +1,9 @@
+let firstname='Shiva'
+let lastname='Prakash'
+fullname=firstname+' '+ lastname
+console.log(fullname)
+
+let firstname1='Shiva'
+let lastname1='Prakash'
+fullname=firstname1.concat(' ',lastname1)
+console.log(fullname)
